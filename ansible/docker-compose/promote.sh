32172cfa8346ce9c2b6c803d36a0c274830e044b
Zoltan Varga <zoltan.varga@netum.fi>
2022-05-19 22:51:02 +0300

    CSC22-5 jenkins, promootio-skriptit

---
+++ ansible/docker-compose/promote.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TAG="$1"
+
+cleanup() {
+    # kill all processes whose parent is this process
+    pkill -P $$
+}
+
+trap cleanup EXIT
+
+function handle {
+  echo "$1 $2 $3"
+  if [ "tag" == "$1" ] && [ "(name=$TAG)" == "$3" ];
+  then
+    date
+    echo "Image $2 was tagged with $TAG, restarting services..."
+    docker compose stop csc2022-app
+    docker compose rm --force csc2022-app
+    docker compose up -d csc2022-app
+  fi
+}
+
+function listen {
+  docker events --filter "type=image" | while read f1 f2 f3 f4 f5; do handle "$f3" "$f4" "$f5"; done
+}
+
+echo "Listening to docker events: tagging an image with $TAG shall restart docker compose services"
+
+listen
