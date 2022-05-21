2d8c16a39eef1d1e688b3b446c49cf3fe6b03cec
Zoltan Varga <zoltan.varga@netum.fi>
2022-05-21 16:07:52 +0300

    Api-sivulla Server-valikossa oli http://koe7-dv... eikä https://, korjattu siten, että URI on /

--- src/main/java/fi/netum/csc/config/OpenApiConfiguration.java
+++ src/main/java/fi/netum/csc/config/OpenApiConfiguration.java
@@ -1,5 +1,6 @@
 package fi.netum.csc.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import tech.jhipster.config.JHipsterConstants;
 import tech.jhipster.config.JHipsterProperties;
 import tech.jhipster.config.apidoc.customizer.JHipsterOpenApiCustomizer;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 @Profile(JHipsterConstants.SPRING_PROFILE_API_DOCS)
@@ -29,5 +31,11 @@ public class OpenApiConfiguration {
             .packagesToScan(API_FIRST_PACKAGE)
             .pathsToMatch(properties.getDefaultIncludePattern())
             .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .addServersItem(new Server().url("/"));
     }
 }
