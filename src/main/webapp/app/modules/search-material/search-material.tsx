e2fea87472dddbb3c8ef54251e042daa19399854
Jermu Mäki <jermu.maki@netum.fi>
2022-05-23 08:18:36 +0300

    Rajausehdot

--- src/main/webapp/app/modules/search-material/search-material.tsx
+++ src/main/webapp/app/modules/search-material/search-material.tsx
@@ -1,22 +1,23 @@
 import './search-material.scss';
 
 import React, {useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {translate, Translate, ValidatedField, ValidatedForm} from 'react-jhipster';
-import {Row, Col, Alert, Button} from 'reactstrap';
+import {Row, Col, Alert, Button, Input, Badge, Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
 
 import {useAppDispatch, useAppSelector} from 'app/config/store';
 import {toast} from "react-toastify";
 import {handleSearch, addFilter, deleteFilter} from "app/modules/search-material/search-material.reducer";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
+
 export const SearchMaterial = () => {
   const account = useAppSelector(state => state.authentication.account);
   const searchparams = useAppSelector(state => state.searchMaterial);
   const dispatch = useAppDispatch();
 
-  const handleValidSubmit = ({ searchterms, filter }) => {
-    dispatch(handleSearch({ searchTerms: searchterms, filters: filter }));
+  const handleValidSubmit = ({searchterms, filter}) => {
+    dispatch(handleSearch({searchTerms: searchterms, filters: filter}));
   };
 
   const handleAddFilter = (params) => {
@@ -29,54 +30,78 @@ export const SearchMaterial = () => {
 
   const rajausehdot = ["Abiturientti", "Alanvaihtaja", "Itsensä sivistäjä", "Työelämän tarpeista opiskeleva", "Yliopiston jatko-opiskelija"];
 
+  const sort = ["Suosituin ensin"];
 
   return (
     <Row>
-      <Col md="9">
+      <Col>
         <h2>
-          <Translate contentKey="materialsearch.title.welcome" interpolate={{ name: account.username }}>
-            Tervetuloa, {account.username}.
+          <Translate contentKey="materialsearch.title.welcome" interpolate={{name: account.firstName}}>
+            Tervetuloa, {account.firstName}.
           </Translate>
         </h2>
         <ValidatedForm id="materialsearch-form" onSubmit={handleValidSubmit} defaultValues={searchparams}>
-          <ValidatedField
-            name="searchterms"
-            label={translate('materialsearch.form.searchterms')}
-            id="searchterms"
-            placeholder={translate('materialsearch.form.searchterms-placeholder')}
-            data-cy="searchterms"
-          />
-          <ValidatedField
-            name="filter"
-            label={translate('materialsearch.form.filter')}
-            id="filter"
-            placeholder={translate('materialsearch.form.filter')}
-            type="select"
-            data-cy="filter"
-            onChange={handleAddFilter}>
-            {
-              [<option selected key="firstElement"> {translate('materialsearch.form.filter')} </option>]
-                .concat(rajausehdot.filter(item => !searchparams.filters.includes(item)).map(rajaus => (
-                  <option value={rajaus} key={rajaus}>
-                    {rajaus}
-                  </option>
-                )))
-            }
-          </ValidatedField>
-          <div className="filterList">
-          {searchparams.filters.map(filter =>
-            <span
-              key={filter}
-              className="badge bg-primary filterChip"
-              onClick={() => handleDeleteFilter(filter)}>
-              {filter}
-              &nbsp;
-              <FontAwesomeIcon icon="trash" />
-            </span>)}
-          </div>
-          <Button color="primary" type="submit" data-cy="submit">
-            <Translate contentKey="materialsearch.form.button">Search</Translate>
-          </Button>
+          <Row>
+            <Col>
+              <Input
+                type="text"
+                name="searchterms"
+                label={translate('materialsearch.form.searchterms')}
+                id="searchterms"
+                placeholder={translate('materialsearch.form.searchterms-placeholder')}
+                data-cy="searchterms"
+              />
+              <Button color="primary" type="submit" data-cy="submit">
+                <Translate contentKey="materialsearch.form.button">Search</Translate>
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              lg="2">
+              <Input
+                name="filter"
+                label={translate('materialsearch.form.filter')}
+                id="filter"
+                placeholder={translate('materialsearch.form.filter')}
+                type="select"
+                data-cy="filter"
+                onChange={handleAddFilter}
+                defaultValue={null}>
+                {
+                  [<option key="firstElement"> {translate('materialsearch.form.filter')} </option>]
+                    .concat(rajausehdot.filter(item => !searchparams.filters.includes(item)).map(rajaus => (
+                      <option value={rajaus} key={rajaus}>
+                        {rajaus}
+                      </option>
+                    )))
+                }
+              </Input>
+            </Col>
+            <Col sm="10" className="filterList">
+              {searchparams.filters.map(filter =>
+                <Badge
+                  key={filter}
+                  className="filterChip"
+                  tabindex="0"
+                  color="primary"
+                  onClick={() => handleDeleteFilter(filter)}
+                  pill>
+                  {filter}
+                  <span className="icon">
+                    <FontAwesomeIcon icon="x"/>
+                  </span>
+                </Badge>)}
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="2">
+              <Input
+                type="select">
+                {sort.map(s => <option key={s}>{s}</option>)}
+              </Input>
+            </Col>
+          </Row>
         </ValidatedForm>
       </Col>
     </Row>
