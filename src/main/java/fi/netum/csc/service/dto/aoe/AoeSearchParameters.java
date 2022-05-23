23bc9712c13740b85e56ff33b44358996b4d12f1
Tommi Sahimäki <tommi.sahimaki@netum.fi>
2022-05-23 14:38:17 +0300

    Poistettu turhia importteja

--- src/main/java/fi/netum/csc/service/dto/aoe/AoeSearchParameters.java
+++ src/main/java/fi/netum/csc/service/dto/aoe/AoeSearchParameters.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 import java.util.List;
