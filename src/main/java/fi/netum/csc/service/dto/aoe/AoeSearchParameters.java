f0fd60beb491a52711ca9a31396db42c94a82ddb
Tommi Sahimäki <tommi.sahimaki@netum.fi>
2022-05-23 18:01:00 +0300

    Turha importti pois

--- src/main/java/fi/netum/csc/service/dto/aoe/AoeSearchParameters.java
+++ src/main/java/fi/netum/csc/service/dto/aoe/AoeSearchParameters.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
