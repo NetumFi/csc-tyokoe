c804c5487ff25ab181e370b9d5b9b4f35b4f7510
Jermu Mäki <jermu.maki@netum.fi>
2022-05-24 15:26:06 +0300

    Sivutus, tyylimuotoilut hakuun

--- src/main/webapp/app/modules/search-material/search-material.tsx
+++ src/main/webapp/app/modules/search-material/search-material.tsx
@@ -1,18 +1,22 @@
 import './search-material.scss';
 
-import React, {useEffect} from 'react';
-import {translate, Translate, ValidatedForm} from 'react-jhipster';
-import {Badge, Button, Col, Container, Input, Row} from 'reactstrap';
+import React, {useEffect, useState} from 'react';
+import {getSortState, JhiItemCount, JhiPagination, translate, Translate, ValidatedForm} from 'react-jhipster';
+import {
+  Row,
+  Col,
+  Button,
+  Input,
+  Badge,
+  Container
+} from 'reactstrap';
 
 import {useAppDispatch, useAppSelector} from 'app/config/store';
-import {
-  addFilter,
-  deleteFilter,
-  handleSearch,
-  setSearchTerms
-} from "app/modules/search-material/search-material.reducer";
+import {reset, handleSearch, addFilter, deleteFilter, setSearchTerms, getDefaultSearchParams} from "app/modules/search-material/search-material.reducer";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {getEntities} from "app/entities/education-level-code-set/education-level-code-set.reducer";
+import {overridePaginationStateWithQueryParams} from "app/shared/util/entity-utils";
+import {ITEMS_PER_PAGE, SORT} from "app/shared/util/pagination.constants";
 import SearchCard from "app/shared/layout/search-card/search-card";
 
 export const SearchMaterial = (props) => {
@@ -22,18 +26,55 @@ export const SearchMaterial = (props) => {
   const currentLocale = useAppSelector(state => state.locale.currentLocale);
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(getEntities({page: 0, size: 100, sort: null}));
-  }, [props.location]);
+  const [paginationState, setPaginationState] = useState(
+    overridePaginationStateWithQueryParams(getSortState(props.location, ITEMS_PER_PAGE, 'relevance'), props.location.search)
+  );
 
-  const filterItems = educationLevelCodeSets.entities;
-
-  const handleValidSubmit = (data) => {
-    dispatch(handleSearch(data));
+  const reloadResults = () => {
+    dispatch(handleSearch({
+      page: paginationState.activePage - 1 || 0,
+      size: paginationState.itemsPerPage,
+      sort: `${paginationState.sort},${paginationState.order}`,
+    }));
   };
 
-  const handleAddFilter = (params) => {
-    const rajaus = filterItems.filter(i => i.codeId === params.target.value)[0];
+  useEffect(() => {
+    reset();
+    dispatch(getEntities({page: 0, size: 999, sort: null}));
+    dispatch(getDefaultSearchParams());
+  }, [props.location.pathname]);
+
+
+  useEffect(() => {
+    if (props.location.search) {
+      reloadResults();
+    }
+  }, [props.location.search, paginationState.activePage, paginationState.order, paginationState.sort]);
+
+  const filterItems = educationLevelCodeSets.entities;
+  useEffect(() => {
+    if (filterItems && searchparams.defaultEducationalLevel) {
+      handleAddFilter(searchparams.defaultEducationalLevel)
+    }
+  }, [filterItems, searchparams.defaultEducationalLevel]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(props.location.search);
+    const page = params.get('page');
+    const sort = params.get(SORT);
+    if (page && sort) {
+      const sortSplit = sort.split(',');
+      setPaginationState({
+        ...paginationState,
+        activePage: +page,
+        sort: sortSplit[0],
+        order: sortSplit[1],
+      });
+    }
+  }, [props.location.search]);
+
+  const handleAddFilter = (uuid) => {
+    const rajaus = filterItems.filter(i => i.codeId === uuid)[0];
     dispatch(addFilter(rajaus));
   };
 
@@ -44,6 +85,19 @@ export const SearchMaterial = (props) => {
   const setInputSearchTerms = (value) => {
     dispatch(setSearchTerms(value.target.value));
   }
+
+  const handlePagination = currentPage => {
+    setPaginationState({
+      ...paginationState,
+      activePage: currentPage,
+    });
+
+    const endURL = `?page=${currentPage}&sort=relevance,${paginationState.order}`;
+    if (searchparams.material.hits && props.location.search !== endURL) {
+      props.history.push(`${props.location.pathname}${endURL}`);
+    }
+  };
+
 
   const codeLocales = {"fi": "labelFi", "en": "labelEn", "sv": "labelSv"};
 
@@ -57,18 +111,17 @@ export const SearchMaterial = (props) => {
           </Translate>
         </h2>
         <Container>
-        <ValidatedForm id="materialsearch-form" onSubmit={handleValidSubmit} defaultValues={searchparams}>
-          <Row>
+        <ValidatedForm id="materialsearch-form" onSubmit={reloadResults} defaultValues={searchparams}>
+          <Row className="mt-3">
             <Col sm="10">
-              <label className={'not-visible-label'}>
-                hakusana:
-              </label>
               <Input
                 type="text"
                 name="searchTerms"
+                label={translate('materialsearch.form.searchterms')}
                 placeholder={translate('materialsearch.form.searchterms-placeholder')}
                 data-cy="searchTerms"
                 onChange={setInputSearchTerms}
+                defaultValue={searchparams.searchTerms}
               />
             </Col>
             <Col xs="1">
@@ -77,11 +130,9 @@ export const SearchMaterial = (props) => {
               </Button>
             </Col>
           </Row>
-          <Row className={'cust-select-limit'}>
-            <Col lg="3">
-              <label className={'not-visible-label'}>
-                raja:
-              </label>
+          <Row className="mt-3">
+            <Col
+              lg="3">
               <Input
                 name="filter"
                 label={translate('materialsearch.form.filter')}
@@ -89,13 +140,11 @@ export const SearchMaterial = (props) => {
                 placeholder={translate('materialsearch.form.filter')}
                 type="select"
                 data-cy="filter"
-                onChange={handleAddFilter}
+                onChange={(params) => handleAddFilter(params.target.value)}
                 defaultValue={null}>
                 {
-                  [
-                    <option key="firstElement"> {
-                    translate('materialsearch.form.filter')} </option>
-                  ].concat(filterItems.filter(item => !searchparams.filters.includes(item)).map(filterItem => (
+                  [<option key="firstElement"> {translate('materialsearch.form.filter')} </option>]
+                    .concat(filterItems.filter(item => !searchparams.filters.includes(item)).map(filterItem => (
                       <option value={filterItem.codeId} key={filterItem.codeId}>
                         {filterItem[codeLocales[currentLocale]]}
                       </option>
@@ -104,9 +153,9 @@ export const SearchMaterial = (props) => {
               </Input>
             </Col>
             <Col sm="9" className="filterList">
-              {searchparams.filters.map(filterItem =>
+              {searchparams.filters.filter(i => i !== undefined).map(filterItem =>
                 <Badge
-                  key={filterItem.codeId}
+                  key={filterItem?.codeId}
                   className="filterChip"
                   tabIndex={0}
                   color="primary"
@@ -119,30 +168,19 @@ export const SearchMaterial = (props) => {
                 </Badge>)}
             </Col>
           </Row>
+          <Row  className="mt-3">
+            <Col lg="3">
+              <Input
+                type="select">
+                {sort.map(s => <option key={s}>{s}</option>)}
+              </Input>
+            </Col>
+          </Row>
         </ValidatedForm>
         </Container>
-
-      <div >
-        <Container className={'cust-seacrh-component'}>
-          <Row>
-            <Col>{searchparams.material?.hits &&
-              <h3 className={"cust-results"}>
-                {searchparams.material?.hits} <Translate contentKey="search.cardseacrhresult"> search results</Translate>
-            </h3>}</Col>
-            {/* eslint-disable-next-line no-console */}
-            {searchparams.material?.hits
-              && searchparams.material?.hits >= 2
-              && <Col className={'cust-select-input'}>
-                <label className={'not-visible-label'}>
-                  Sorting:
-                </label>
-                <Input type="select" name="select" id="selectSorting" data-cy="selectSorting">
-                  {sort.map(s => <option key={s}>{s}</option>)}
-                </Input>
-              </Col>}
-          </Row>
-
-        </Container>
+      <div  className="mt-3" />
+      {searchparams.material?.hits && <p>Hakutuloksia {searchparams.material?.hits}</p>}
+      <div className="results">
         {searchparams.material?.results.map(result => {
           return (
             <SearchCard key={result.key}
@@ -152,6 +190,22 @@ export const SearchMaterial = (props) => {
                         description={d => d.description} />)
         })}
       </div>
+      {searchparams.material?.results && searchparams.material?.results?.length > 0 &&
+      <div className={searchparams.material?.results && searchparams.material?.results?.length > 0 ? '' : 'd-none'}>
+        <div className="justify-content-center d-flex">
+          <JhiItemCount page={paginationState.activePage} total={searchparams.material?.hits} itemsPerPage={paginationState.itemsPerPage} i18nEnabled />
+        </div>
+        <div className="justify-content-center d-flex">
+          <JhiPagination
+            activePage={paginationState.activePage}
+            onSelect={handlePagination}
+            maxButtons={5}
+            itemsPerPage={paginationState.itemsPerPage}
+            totalItems={searchparams.material?.hits}
+          />
+        </div>
+      </div>
+      }
     </Container>
   );
 };
