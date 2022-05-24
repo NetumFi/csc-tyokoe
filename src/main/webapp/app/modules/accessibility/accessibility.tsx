fbf82fc8b73cbca35da5f8606d6998badc4e785f
Mohamid Shaker <70940766+Moha-Sha@users.noreply.github.com>
2022-05-24 14:50:00 +0300

    haku component korjaukset

--- src/main/webapp/app/modules/accessibility/accessibility.tsx
+++ src/main/webapp/app/modules/accessibility/accessibility.tsx
@@ -3,9 +3,23 @@ import React from 'react';
 const Accessibility = () => {
   return (
   <div className={"fav-level1"}>
-    <h1>
+
       <h1>Accessibility</h1>
-    </h1>
+
+    <p>Kestävä elämä -podcast-sarja pureutuu vastuullisuuden eri osa-alueisiin niin arkielämässä kuin
+      liiketoiminnassa, pyrkien löytämään vastauksia siihen, miten jokainen voi elää helposti kestävämmin.
+      Podcastin ympärille on luotu erityisesti toisen asteen opiskelijoille suunnattu oppimateriaalikokonaisuus,
+      joka käsittää kysymyksiä sekä visailutehtäviä jokaisen jakson aihepiiriin liittyen. Tehtävät soveltuvat hyvin
+      niin etäopiskeluun kuin tuntimateriaaliksi useissa eri oppiaineissa, mutta niitä voi hyödyntää myös taustana omien
+      kulutusvalintojen pohtimiseen ja itsenäisen opiskelun tueksi. Jaksokohtaisia kysymyksiä voi
+      käyttää pohjana ja soveltaa eri oppiaineisiin ja eri alojen opetukseen tai tuntikehykseen sopivaksi.
+      Tehtävät sopivat hyvin aineistoksi esimerkiksi useampia aineita yhdisteleviin kokonaisuuksiin ja
+      ilmiöoppimiseen, yhteisen ja laaja-alaisen osaamisen oppimiseen. Kestävä elämä -podcastin oppimiskokonaisuus on
+      toteutettu yhteistyössä Opetushallituksen, Sitran, YK-liiton, Perho Liiketalousopiston ja Marttaliiton edustajista
+      koostuvan toimitusneuvoston kanssa.
+
+    </p>
+
   </div>
   );
 };
--- src/main/webapp/app/shared/layout/search-card/search-card.tsx
+++ src/main/webapp/app/shared/layout/search-card/search-card.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Badge} from "reactstrap";
 import {TextFormat} from "react-jhipster";
 import {APP_SUOMI_DATE_FORMAT} from "app/config/constants";
+import {Link} from "react-router-dom";
 
 const SearchCard = (props: {
   result: any,
   materialName: (m) => string,
   lang: (d) => string,
   description: (d) => string}) => {
+  const aoeurl = 'https://aoe.fi/#/materiaali/';
 
   return <div className="card">
     <div className="row no-gutters">
@@ -17,9 +19,12 @@ const SearchCard = (props: {
       </div>
       <div className="col">
         <div className="card-block px-2">
-          <h4 className="cust_card-title">
-            {props.result.materialName.filter(m => m.language === props.lang).map(x => x.materialname)[0]}
-          </h4>
+            <Link to={{ pathname: aoeurl + props.result.id}} target="_blank" className={'cust-link'}>
+              <h4 className="cust_card-title">
+                {props.result.materialName.filter(m => m.language === props.lang).map(x => x.materialname)[0]}
+              </h4>
+            </Link>
+
           <p className="card-text">
             {props.result.description.filter(d => d.language === props.lang).map(d => d.description)[0]}
           </p>
@@ -28,11 +33,15 @@ const SearchCard = (props: {
               <Badge
                 className={'cust_Badge'}
                 key={t.learningresourcetypekey}
-                color="secondery"
                 pill>
                 {t.value}
               </Badge>)}
-            <p className={'date_text'}>{props.result && props.result?.updatedAt ? <TextFormat type="date" value={props.result.updatedAt} format={APP_SUOMI_DATE_FORMAT} /> : null}</p>
+            <p className={'date_text'}>
+              {props.result &&
+              props.result?.updatedAt ?
+                <TextFormat type="date"
+                            value={props.result.updatedAt}
+                            format={APP_SUOMI_DATE_FORMAT} /> : null}</p>
           </div>
         </div>
       </div>
