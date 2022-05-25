4c06162890c806cf58f0514712cc88fdeced916e
Jouko Johansson <jouko.johansson@netum.fi>
2022-05-25 09:48:52 +0300

    korjattu saavutettavauus + poistettu noscript-tagi

--- src/main/webapp/app/modules/accessibility/accessibility.tsx
+++ src/main/webapp/app/modules/accessibility/accessibility.tsx
@@ -4,16 +4,16 @@ const Accessibility = () => {
   return (
   <div className={"fav-level1"}>
 
-    <h4>Saavutettavuusseloste</h4>
+    <h1 className="h3">Saavutettavuusseloste</h1>
     <p>Palvelun saavutettavuuden arviointi on suoritettu itsearviointina.
     Saavutettavuusvaatimuksia koskevat säännökset ovat Digitaalisten palveluiden tarjoamista koskevassa laissa.
     Tämä saavutettavuusseloste on laadittu 25.5.2022.
       </p>
 
-    <h5>Palvelun saavutettavuus</h5>
+    <h2 className="h4">Palvelun saavutettavuus</h2>
     <p>Palvelu täyttää saavutettavuusvaatimukset  (WCAG-kriteeristö 2.1, A- ja AA-taso).</p>
 
-    <h5>Anna palautetta</h5>
+    <h2 className="h4">Anna palautetta</h2>
 
     <p>Jos löydät sivustolta ongelmia joita ei ole kuvattu tässä selosteessa, ilmoita niistä meille. Otamme mielellämme vastaan myös muita huomioita ja parannusehdotuksia sivuston saavutettavuuteen liittyen. Voit lähettää palautteesi sähköpostilla myynti(at)netum. Kerro palautteessasi, millä sivulla/sivuilla havaitsit ongelman ja kuvaa ongelma mahdollisimman tarkasti.</p>
 
